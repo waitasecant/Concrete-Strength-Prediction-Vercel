5d3cd11bc6aab7e482eb3d1c95674238b333a2ef
Himanshu <waitasecant@gmail.com>
2024-04-22 14:47:23 +0530

    Initial Commit

---
+++ app.py
@@ -0,0 +1,42 @@
+from flask import Flask, render_template, request
+import pickle
+import pandas as pd
+import numpy as np
+
+app = Flask(__name__)
+model = pickle.load(open("RFModel.pkl", "rb"))
+
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    return render_template("index.html")
+
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    Cement = request.form.get("Cement")
+    Blast_Furnace_Slag = request.form.get("Blast_Furnace_Slag")
+    Fly_Ash = request.form.get("Fly_Ash")
+    Superplasticizer = request.form.get("Superplasticizer")
+    Age = request.form.get("Age")
+    prediction = model.predict(
+        pd.DataFrame(
+            columns=[
+                "Cement",
+                "Blast_Furnace_Slag",
+                "Fly_Ash",
+                "Superplasticizer",
+                "Age",
+            ],
+            data=np.array(
+                [Cement, Blast_Furnace_Slag, Fly_Ash, Superplasticizer, Age]
+            ).reshape(1, 5),
+        )
+    )
+    return (
+        str(np.round(prediction[0], 3)) + " " + "\N{PLUS-MINUS SIGN}" + " " + str(5.825)
+    )
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=4000, debug=True)
